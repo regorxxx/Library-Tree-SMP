7619dd4980768c7e4d4e016934f670827e118113
regorxxx <regorxxx@protonmail.com>
2026-01-30 14:00:17 +0100

    Bom

--- _createPackage.bat
+++ _createPackage.bat
@@ -1,4 +1,4 @@
-﻿@ECHO off
+@ECHO off
 REM ------------------------------------------------------------------
 REM Create packages (zip file) from js files v.28/01/2026
 REM Requires 7za.exe on windows to compress (otherwise do it manually)
